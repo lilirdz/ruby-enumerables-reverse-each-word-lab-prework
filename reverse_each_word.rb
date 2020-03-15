9c3e00bd2e933cd80d9211e9be227b531b3ffe2c
Lili Rodriguez <lilimrod@gmail.com>
2020-03-15 22:13:30 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,11 @@
+def reverse_each_word (array)
+  new_array = array.split(" ")
+  new_array.each {|x| x.reverse!}
+  return new_array.join(" ")
+end
+
+def reverse_each_word(array)
+  new_array = array.split(" ")
+  new_array.collect {|x| x.reverse!}
+  return new_array.join(" ")
+end
