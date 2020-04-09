8468b021c517a86193d4239173468d5d0b3695ad
Lili Rodriguez <lilimrod@gmail.com>
2020-04-09 05:13:28 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,11 +7,6 @@ def reverse_each_word_with_each(string)
   return new_array.join(" ")
 end
 
-def reverse_each_word(string)
-  array = string.split(" ") 
-  new_array = []
-  array.collect do |string|
-  new_array.push(string.reverse)
-  end
-  return new_array.join(" ")
+def reverse_each_word(sentence)
+  sentence.split.collect {|word| word.reverse}.join(" ")
 end
